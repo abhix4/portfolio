b746b47cc68e809ebb44a3664637490c82fc5b6c
abhi <abhiifour@gmail.com>
2026-01-11 21:06:16 +0530

    fix: overflow issue - opensource

--- app/_components/OpenSource.tsx
+++ app/_components/OpenSource.tsx
@@ -14,7 +14,7 @@ export default function Opensource({
     <div className="flex justify-between items-start relative py-4 sm:items-start ">
       <div className="w-[1px] h-[100%] bg-neutral-200 absolute left-1.5 -translate-y-4 "></div>
       <div className="flex   gap-3 sm:gap-2 sm:items-start items-start ">
-        <BiGitPullRequest size={16} className="mt-1 text-neutral-600" />
+        <BiGitPullRequest size={16} className="mt-1 text-neutral-600 z-20" />
 
         <div className=''>
           <p className="font-mono text-sm hover:underline text-neutral-800">
